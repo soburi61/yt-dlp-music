5cb5ac85d52c1f69ecd28e4327ba37c2f8726ea6
soburi <591959ant@gmail.com>
2023-04-05 06:26:32 +0900

    add ffmpeg and mkdir yt-dlp

--- env/yt-dlp-music.py
+++ env/yt-dlp-music.py
@@ -16,10 +16,11 @@ while(True):
         sys.exit()
     
     url=input("youtube url(list ok):")
+    cmd = f".\yt-dlp\yt-dlp.exe -x -f ba -i --ffmpeg-location ./ffmpeg/ffmpeg.exe --parse-metadata 'artist:%(channel)s' -o {path:s}/%(title)s.%(ext)s --add-metadata --audio-format mp3 "
     if "list=" in url:
         end = int(input("end-list(example:3→download the three lastest music):"))
-        cmd = f"yt-dlp.exe -x -f ba -i --parse-metadata 'artist:%(channel)s' -o {path:s}/%(title)s.%(ext)s --playlist-end {end} --add-metadata --audio-format mp3 "+url
+        cmd = cmd + f'--playlist-end {end}' + url
     else:
-        cmd = f"yt-dlp.exe -x -f ba -i --parse-metadata 'artist:%(channel)s' -o {path:s}/%(title)s.%(ext)s --add-metadata --audio-format mp3 "+url
+        cmd += url
     subprocess.run('echo '+cmd,shell=True)
     subprocess.run(cmd,shell=True)
