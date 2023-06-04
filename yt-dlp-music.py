f50af66fccd9c3f59fbdeb0d24f7691c8dac99b6
soburi <591959ant@gmail.com>
2023-06-05 03:37:23 +0900

    make changed

--- yt-dlp-music.py
+++ yt-dlp-music.py
@@ -1,26 +1,39 @@
 # -*- coding: utf-8 -*-
 """
 Created on 2023/4/4
-
+Edited on 2023/4/17
+Make Changed on 2023/6/5
 @author: soburi
 """
-import sys, os,subprocess
+import sys, subprocess
 while(True):
-    path='.'
-    #path.txtに記載してあるパスに保存
-    try:
-        with open('path.txt',mode='r') as f:
-            path = f.readline()
-    except:
-        print('nothing text file')
-        sys.exit()
+    path = input("(Default:[Enter] →~/Music/Music)\nTarget path:") or "~/Music/Music"
+
+    if (path == "end"):
+        break
+
+    url = input("(play list url ok)\n(Default:[Enter]→import music.txt)\nmovie url:")
+    cmd = f".\yt-dlp\yt-dlp.exe --ffmpeg-location ./ffmpeg/ffmpeg.exe --parse-metadata artist:%(channel)s -o {path:s}/%(title)s -f b --add-metadata --extract-audio --embed-thumbnail --audio-format mp3 "
     
-    url=input("youtube url(list ok):")
-    cmd = f".\yt-dlp\yt-dlp.exe -x -f ba -i --ffmpeg-location ./ffmpeg/ffmpeg.exe --parse-metadata 'artist:%(channel)s' -o {path:s}/%(title)s.%(ext)s --add-metadata --audio-format mp3 "
-    if "list=" in url:
-        end = int(input("end-list(example:3→download the three lastest music):"))
-        cmd = cmd + f'--playlist-end {end} ' + url
+    list = []
+    if "https://" in url:
+        tmp = url
+        if "list=" in url:
+            #TODO: もしlistだけではなくvideoも指定してしまっていたら正規表現など使って改変を行う。 (ニコニコなども考慮する)
+            end = int(input("(example:3→download the 3 lastest music)\n(Default:[Enter]→download all)\n end num:") or 0)
+            if end != 0:
+                tmp = f"--playlist-end {end} --yes-playlist {url}"
+        list.append(tmp)
+
     else:
-        cmd += url
-    subprocess.run('echo '+cmd,shell=True)
-    subprocess.run(cmd,shell=True)
+        try:
+            with open('music.txt', mode='r') as f:
+                list = f.readlines()
+        except:
+            print("nothing text file.")
+            sys.exit(1)
+    
+    for urls in list:
+        subprocess.run('echo '+cmd+urls, shell=True)
+        subprocess.run(cmd+urls, shell=True) 
+    
